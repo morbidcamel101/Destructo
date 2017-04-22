d09a7d3657892026ec777a3b1e48176ad3a005ce
mrkenz0 <ken.huynh.7@gmail.com>
2017-04-22 01:03:00 -0700

    Forgot to include these files in previous check-in

---
+++ LD38SmallWorld/Assets/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGameButton(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void LoadGameButton(string loadGameLevel)
+    {
+        SceneManager.LoadScene(loadGameLevel);
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsButton(string loadGameLevel)
+    {
+        // Dev Team Credits Page
+    }
+
+}
