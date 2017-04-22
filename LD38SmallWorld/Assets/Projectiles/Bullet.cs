b1c18574de21abc067f31fdd7d1595d2771b75f1
JohanOtto <johan.otto@me.com>
2017-04-22 01:10:28 -0500

    Busy with bullets

---
+++ LD38SmallWorld/Assets/Projectiles/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[AddComponentMenu("Small World/Bullet")]
+public class Bullet : BehaviorBase
+{
+
+	public float speed = 10;
+	public float lifeTime = 0.5f;
+	public float distance = 10000;
+
+	private Transform trans;
+	private float spawnTime;
+
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		trans = transform;
+		spawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		trans.position += trans.forward * speed * Time.deltaTime;
+		distance -= speed * Time.deltaTime;
+		if (Time.time > spawnTime + lifeTime || distance < 0)
+		{
+			// TODO - Recyce
+		}
+	
+	}
+}
+
---
+++ LD38SmallWorld/Assets/Utilities/Spawner.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[AddComponentMenu("Small World/Spawner")]
+public class Spawner : BehaviorBase
+{
+	#region Definitions
+	public class ActiveObject {
+
+		public ActiveObject(GameObject obj) {
+			this.gameObject = obj;
+		}
+
+		public GameObject gameObject;
+
+		public bool active;
+
+		public float recycleTime;
+	}
+
+	[Serializable]
+	public class ObjectCache {
+		public GameObject prefab;
+		public int cacheSize = 10;
+		public int cacheIndex = 0;
+		public GameObject root;
+		[HideInInspector]
+		public GameObject[] objects;
+
+
+		public void Initialize()
+		{
+			objects = new GameObject[cacheSize];
+
+			for(var i = 0; i < cacheSize; i++)
+			{
+				var obj = objects[i];
+				obj = Instantiate(prefab) as GameObject;
+				obj.SetActive(true);
+				obj.name = prefab.name + "(" + i.ToString() + ")";
+			}
+		}
+
+		public GameObject Next()
+		{
+			GameObject obj = null;
+			for(int i = 0; i < cacheSize; i++)
+			{
+				obj = objects[i];
+
+				if (obj == null || !obj.activeSelf || obj)
+					break;
+
+				cacheIndex = (cacheIndex + 1) % cacheSize;
+			}
+			if (obj == null)
+					return null;
+
+			cacheIndex = (cacheIndex + 1) % cacheSize;
+			return obj;
+		}
+
+	}
+	#endregion
+
+	#region Behavior
+
+	public static Spawner spawner;
+	// Use this for initialization
+	void Awake ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	#endregion
+}
+
