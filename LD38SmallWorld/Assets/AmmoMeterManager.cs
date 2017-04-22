95e1ad79e66a90ae4bb333782e8473d3a88ab350
mrkenz0 <ken.huynh.7@gmail.com>
2017-04-22 15:45:42 -0700

    Health and ammo meters are now working via test buttons. Need to hook into game play action.

--- LD38SmallWorld/Assets/AmmoMeterManager.cs
+++ LD38SmallWorld/Assets/AmmoMeterManager.cs
@@ -6,17 +6,44 @@ using UnityEngine.UI;
 
 public class AmmoMeterManager : MonoBehaviour
 {
-    private int fillAmount;
+    private float fillAmount;
+
+    public Image content;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        fillAmount = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        HandleAmmoMeterDisplay();
+    }
+
+    private void HandleAmmoMeterDisplay()
+    {
+        content.fillAmount = AmmoFillAmount(fillAmount, 0, 100, 0, 1);
+    }
+
+    private float AmmoFillAmount(float healthVal, float inMinHealthVal, float inMaxHealthVal, float outMinFillVal, float outMaxFillVal)
+    {
+        return (healthVal - inMinHealthVal) * (outMaxFillVal - outMinFillVal) / (inMaxHealthVal - inMinHealthVal) + outMinFillVal;
+    }
+
+    public void AmmoIncrease()
+    {
+        // Test relaod back to full capacity
+        fillAmount = 100;
+    }
+
+    public void AmmoDecrease()
+    {
+        // Test
+        fillAmount = fillAmount - 2;
+
+        if (fillAmount < 0)
+            fillAmount = 0;
+    }
 }
--- LD38SmallWorld/Assets/HealthMeterManager.cs
+++ LD38SmallWorld/Assets/HealthMeterManager.cs
@@ -1,22 +1,51 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthMeterManager : MonoBehaviour
 {
-    private int fillAmount;
+    private float fillAmount;
+    
+    public Image content;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        fillAmount = 100;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        HandleHealthMeterDisplay();
+    }
+
+    private void HandleHealthMeterDisplay()
+    {
+        content.fillAmount = HealthFillAmount(fillAmount, 0, 100, 0, 1);
+    }
+
+    private float HealthFillAmount(float healthVal, float inMinHealthVal, float inMaxHealthVal, float outMinFillVal, float outMaxFillVal)
+    {
+        return (healthVal - inMinHealthVal) * (outMaxFillVal - outMinFillVal) / (inMaxHealthVal - inMinHealthVal) + outMinFillVal;
+    }
+
+    public void HealthIncrease()
+    {
+        // Test
+        fillAmount = fillAmount + 5;
+
+        if (fillAmount > 100)
+            fillAmount = 100;
+    }
+
+    public void HealthDecrease()
+    {
+        // Test
+        fillAmount = fillAmount - 5;
+
+        if (fillAmount < 0)
+            fillAmount = 0;
+    }
 }
