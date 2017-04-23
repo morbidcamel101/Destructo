38ed999081dfb0209c712f2b6dbe78cfe0882b14
mrkenz0 <ken.huynh.7@gmail.com>
2017-04-23 01:23:00 -0700

    Created prefab for HUD

--- LD38SmallWorld/Assets/AI/Health.cs
+++ LD38SmallWorld/Assets/AI/Health.cs
@@ -12,12 +12,7 @@ public sealed class Health: BehaviorBase
 	public float criticalPercentage = 0.2f;
 	public float lowPercentage = 0.5f;
 	public bool dead = false;
-
-    public Image healthBar;
-    public GameObject[] bloodOverlayObjects;
-
-    private bool showBloodOverlay = false;
-
+    
     void Awake()
 	{
 		enabled = false;
@@ -56,87 +51,5 @@ public sealed class Health: BehaviorBase
 		currentHealth += regenerationRate;
 		yield return new WaitForSeconds(regenerationRate);
 	}
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        HandleHealthMeterDisplay();
-    }
-
-    private void HandleHealthMeterDisplay()
-    {
-        healthBar.fillAmount = HealthFillAmount(currentHealth, 0, totalHealth, 0, 1);
-
-        if (healthBar.fillAmount <= lowPercentage && healthBar.fillAmount > criticalPercentage)
-        {
-            healthBar.color = Color.yellow;
-            showBloodOverlay = false;
-        }
-        else if (healthBar.fillAmount <= criticalPercentage)
-        {
-            healthBar.color = Color.red;
-            showBloodOverlay = true;
-        }
-        else
-        {
-            healthBar.color = Color.green;
-            showBloodOverlay = false;
-        }
-
-        HandleBloodOverlay();
-    }
-
-    private void HandleBloodOverlay()
-    {
-        foreach (GameObject go in bloodOverlayObjects)
-        {
-            go.SetActive(showBloodOverlay);
-        }
-    }
-
-    IEnumerator HandleRegenerationFactor()
-    {
-        while (true)
-        {
-            // Loops forever...
-            if (currentHealth < totalHealth)
-            {
-                // If health < total max...
-                currentHealth += 1;
-
-                // Increase health and wait the specified time
-                yield return new WaitForSeconds(1);
-            }
-            else
-            {
-                // If health >= total max, just yield 
-                yield return null;
-            }
-        }
-    }
-
-    private float HealthFillAmount(float healthVal, float inMinHealthVal, float inMaxHealthVal, float outMinFillVal, float outMaxFillVal)
-    {
-        return (healthVal - inMinHealthVal) * (outMaxFillVal - outMinFillVal) / (inMaxHealthVal - inMinHealthVal) + outMinFillVal;
-    }
-
-    public void HealthIncrease()
-    {
-        // Test
-        currentHealth = currentHealth + 5;
-
-        if (currentHealth > totalHealth)
-            currentHealth = totalHealth;
-    }
-
-    public void HealthDecrease()
-    {
-        // Test
-        currentHealth = currentHealth - 5;
-
-        if (currentHealth < 0)
-            currentHealth = 0;
-    }
 }
 
